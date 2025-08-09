7345bbaf234d7da3c9117d9ddb1b30e62eccb798
Mohammed Salamah <msalamah259@gmail.com>
2025-08-09 23:06:14 +0800

    search page done

--- webpack.config.js
+++ webpack.config.js
@@ -90,9 +90,18 @@ module.exports = {
       filename: "payment.html",
       template: "./src/payment.html"
     }),
+    new HtmlWebpackPlugin({
+      filename: "search.html",
+      template: "./src/search.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "./src/contact.html"
+    }),
     new MiniCssExtractPlugin({
       filename: "css/style.css"
     }),
     new CssMinimizerPlugin()
   ]
 }; 
+
